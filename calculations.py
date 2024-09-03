e6aba99a6d8a7949607c57a7204212a9261a9462
Sunelle Botha <sunellebo97@gmail.com>
2024-09-03 19:54:57 +0200

    Added a script that takes two numbers as an input and performs calculations with the numbers.

---
+++ calculations.py
@@ -0,0 +1,7 @@
+# Request the user to enter two numbers.
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print(f"The sum of {num1} and {num2} is {num1 + num2}")
+print(f"The difference between {num1} and {num2} is {num1 - num2}")
+print(f"The product of {num1} and {num2} is {num1 * num2}")
